2d84b4f83d8d2351b38dab674cdc600e850aacbc
Josip Marković <josip.markovic@infinum.hr>
2016-09-09 21:04:30 +0200

    Revert comment from MainActivity

--- app/src/main/java/pub/devrel/easypermissions/sample/MainActivity.java
+++ app/src/main/java/pub/devrel/easypermissions/sample/MainActivity.java
@@ -101,8 +101,7 @@ public class MainActivity extends AppCompatActivity implements
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        // EasyPermissions handles the request result with an
-        // example when the {@code object} instance is not an Activity or Fragment.
+        // EasyPermissions handles the request result.
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 
